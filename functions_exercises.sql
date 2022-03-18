0530fc0abbb312e63a9ba2905ee4a7713e1df474
Cody Hodges <hodges.cody00@gmail.com>
2022-03-18 08:52:39 -0500

    SQL Exercises

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -67,3 +67,13 @@ FROM employees
 WHERE year(hire_date) BETWEEN 1990 AND 1999
   AND month(birth_date) = 12
   AND day(birth_date) = 25;
+
+
+# Change the query for employees hired in the 90s and born on Christmas such that the first result is the oldest employee who was hired last.
+# It should be Khun Bernini.
+SELECT first_name, last_name, birth_date, hire_date
+FROM employees
+WHERE year(hire_date) BETWEEN 1990 AND 1999
+  AND month(birth_date) = 12
+  AND day(birth_date) = 25
+ORDER BY year(birth_date), year(hire_date) DESC;
